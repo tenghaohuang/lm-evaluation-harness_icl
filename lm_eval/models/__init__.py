ca334670c204c451178860f7b9a1300d04962bdb
VictorSanh <victorsanh@gmail.com>
2021-05-27 19:17:46 +0000

    addDecoderOnlyT5LM

--- lm_eval/models/__init__.py
+++ lm_eval/models/__init__.py
@@ -1,10 +1,12 @@
 from . import gpt2
 from . import gpt3
 from . import dummy
+from . import dec_only_t5
 
 MODEL_REGISTRY = {
     "gpt2": gpt2.GPT2LM,
     "gpt3": gpt3.GPT3LM,
+    "deconlyt5": dec_only_t5.DecoderOnlyT5LM,
     "dummy": dummy.DummyLM,
 }
 
